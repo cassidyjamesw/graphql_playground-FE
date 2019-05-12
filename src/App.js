e255fd48211fec278e170b355a01dc29a1c673fc
James Cassidy <cassidyjamesw@gmail.com>
2019-05-12 14:50:18 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import { ApolloProvider } from "react-apollo";
 // components
 import BookList from "./components/BookList";
 
-// apollo client setup
-// const client = new ApolloClient({
-//   uri: "http://localhost:4000/graphql"
-// });
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      // <ApolloProvider client={client}>
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
-      // </ApolloProvider>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
